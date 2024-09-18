5d5b9577488498ba50f9a91296d096c8fe543295
Zrazhevskii <vbn-134@yandex.ru>
2024-09-18 13:33:09 +0300

    создание структуры

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 // import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import PageNotFound from './pages/PageNotFound/index';
+import Layout from './components/Layout/index';
 
 function App() {
     // const [count, setCount] = useState(0);
 
-    return <div />;
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,6 @@
+// import React from 'react';
+import './PageNotFound.css';
+
+export default function PageNotFound() {
+    return <div>PageNotFound</div>;
+}
