f13b6d48cf1953a683e3374b652f2990fbbf82c5
Zrazhevskii <vbn-134@yandex.ru>
2024-09-18 15:32:25 +0300

    подключение шрифтов

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header__title__box">
+                <span>Realworld Blog</span>
+            </div>
+        </div>
+    );
+}
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,13 @@
 // import React from 'react'
 import './Layout.css';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
 
 export default function Layout() {
-    return <div>Layout</div>;
+    return (
+        <div className="wrapper__app">
+            <Header />
+            <Outlet />
+        </div>
+    );
 }
