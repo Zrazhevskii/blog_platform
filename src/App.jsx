e1ef78a5c6f806fff4548a159bfeeffc78219800
Zrazhevskii <vbn-134@yandex.ru>
2024-09-23 16:36:46 +0300

    добавление SignIn SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import PageNotFound from './pages/PageNotFound/index';
 import Layout from './components/Layout/index';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+// import Header from './components/Header';
 
 function App() {
     // const [count, setCount] = useState(0);
@@ -10,6 +13,8 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
+                <Route path="sign-up" element={<SignUp />} />
+                <Route path="sign-in" element={<SignIn />} />
                 <Route path="*" element={<PageNotFound />} />
             </Route>
         </Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 // import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
@@ -6,6 +7,14 @@ export default function Header() {
         <div className="header">
             <div className="header__title__box">
                 <span>Realworld Blog</span>
+            </div>
+            <div className="header__links__box">
+                <NavLink to="/sign-in" className="header__link">
+                    Sign In
+                </NavLink>
+                <NavLink to="/sign-up" className="header__link">
+                    Sign Up
+                </NavLink>
             </div>
         </div>
     );
