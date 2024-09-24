4d95988353d787d3d6d217324a8b2d2f84fef11f
Zrazhevskii <vbn-134@yandex.ru>
2024-09-24 11:27:04 +0300

    структура post

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,19 +1,43 @@
 import './Post.css';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
 export default function Post({ elem }) {
-    const { title, favoritesCount } = elem;
-    // console.log(favoritesCount);
-    console.log(elem);
+    const { title, favoritesCount, tags, description, createdAt, author } = elem;
+    const { username, image } = author;
+    const user = username
+        .split(' ')
+        .map((el) => el.slice(0, 1).toUpperCase() + el.slice(1))
+        .join(' ');
+    // console.log(user);
+    const date = createdAt ? format(createdAt, 'MMMM dd, yyyy') : 'Дата неуказана';
+
     return (
-        <div className="post__box">
+        <section className="post__box">
             <div className="post__box__content">
                 <div className="post__box__title__box">
                     <h4 className="post__box__title">{title}</h4>
                     <div className="post__box__favorites">{favoritesCount}</div>
                 </div>
+                <div className="post__box__tags__box">
+                    {tags.map((item) => {
+                        return (
+                            <span key={item} className="post__box__tag">
+                                {item}
+                            </span>
+                        );
+                    })}
+                </div>
+                <div className="post__box__description">{description.split(' ').slice(0, 10).join(' ')}</div>
             </div>
-        </div>
+            <div className="post__box__author__box">
+                <div className="post__box__name__box">
+                    <span className="post__box__name">{user}</span>
+                    <span className="post__box__created">{date}</span>
+                </div>
+                <img src={image} alt="автор" className="post__box__img" />
+            </div>
+        </section>
     );
 }
 
@@ -21,5 +45,12 @@ Post.propTypes = {
     elem: PropTypes.shape({
         title: PropTypes.string,
         favoritesCount: PropTypes.number,
+        tags: PropTypes.arrayOf(PropTypes.string),
+        description: PropTypes.string,
+        createdAt: PropTypes.string,
+        author: PropTypes.shape({
+            username: PropTypes.string,
+            image: PropTypes.string,
+        }),
     }),
 };
